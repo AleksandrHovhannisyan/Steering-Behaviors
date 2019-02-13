f049d9f02ed0f66e1c2c5b4de915e73efba438ad
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-02-13 16:12:29 -0500

    Add scripts for controlling separation group behavior.

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    private float desiredSeparation;
+    public int maxSpeed = 60;
+    public int maxForce = 40;
+    private Rigidbody body;
+
+
+    /* Set up all necessary members.
+     */ 
+    private void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody>();
+
+        float radius = transform.localScale.x / 2;
+
+        desiredSeparation = radius * radius;
+    }
+
+
+    /* Called each frame by the group manager. Identifies boids in this boid's neighborhood
+     * and applies an appropriate force to separate it from the others and prevent collision.
+     */ 
+    public void Separate()
+    {
+        Vector3 sum = Vector3.zero;
+        int numNeighbors = 0;
+
+        // Loop through all boids in the world
+        foreach(GameObject vehicle in GroupManager.boids)
+        {
+            Boid boid = vehicle.GetComponent<Boid>();
+
+            Vector3 diff = transform.position - boid.transform.position;
+            float distance = Vector3.Magnitude(diff);
+
+            // If it's a neighbor within our vicinity
+            if(distance > 0 && distance < desiredSeparation)
+            {
+                diff.Normalize();
+
+                // The closer a neighbor (smaller the distance), the more we should flee
+                diff /= distance;
+
+                sum += diff;
+                numNeighbors++;
+            }
+        }
+
+        // That is, if this boid actually has neighbors to worry about
+        if(numNeighbors > 0)
+        {
+            // Compute its average separation vector
+            Vector3 avgDirection = sum / numNeighbors;
+            avgDirection.Normalize();
+            avgDirection *= maxSpeed;
+
+            // Compute the steering force we need to apply
+            Vector3 steeringForce = avgDirection - body.velocity;
+
+            // Cap that steering force
+            if (steeringForce.magnitude > maxForce)
+            {
+                steeringForce.Normalize();
+                steeringForce *= maxForce;
+            }
+
+            // Apply steering force
+            body.AddForce(steeringForce);
+        }
+    }
+
+
+    /* For testing, to ensure there are no collisions.
+     */ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.CompareTag("Boid"))
+        {
+            Debug.Log("Oopsie woopsie");
+        }
+    }
+}
---
+++ Assets/Scripts/GroupManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupManager : MonoBehaviour
+{
+    public static int numVehicles = 150;
+    public static List<GameObject> boids;
+    public GameObject boidPrefab;
+
+    private float minX = 200;
+    private float minZ = 200;
+    
+
+    void Awake()
+    {
+        boids = new List<GameObject>(numVehicles);
+
+        for(int i = 0; i < numVehicles; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-minX, minX), 5, Random.Range(-minZ, minZ));
+            GameObject newBoid = Instantiate(boidPrefab, position, Quaternion.identity);
+            boids.Add(newBoid);
+        }        
+    }
+
+
+    void FixedUpdate()
+    {
+        foreach(GameObject boid in boids)
+        {
+            boid.gameObject.GetComponent<Boid>().Separate();
+        }
+    }
+}
